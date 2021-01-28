fe1be2807362888849a8d1b0c3e5ba5448dd3ca3
Tommy Carpenter <tommyjcarpenter@gmail.com>
2021-01-28 14:46:55 -0500

    Shell agnostic, pip, use ENV for SHELL,HOME

--- bootstrap/boot.py
+++ bootstrap/boot.py
@@ -4,20 +4,11 @@ from bootstrap.utils import (
     mkdirs,
     softlinks,
     vim,
-    pipinstall,
     packages,
 )
 
 
 def boot(cfg: dict, name, redovim, systype, loctype):
-    if systype == "mac":
-        utils.BOOTSTRAPHOMEDIR = "/Users/{}".format(name)
-        utils.BOOTSTRAPFISHPATH = "/usr/local/bin/fish"
-
-    if systype == "arch":
-        utils.BOOTSTRAPHOMEDIR = "/home/{}".format(name)
-        utils.BOOTSTRAPFISHPATH = "/usr/bin/fish"
-
     # CURRENT ORDER (TODO, make this specifiable??)
     # 1. mkdirs
     # 2. generic softlinks
@@ -32,17 +23,12 @@ def boot(cfg: dict, name, redovim, systype, loctype):
     cmds(cfg, "all")  # should cmds just do this too at the end?
     packages(cfg, systype)
 
-    # TODO: ALL IOF THIS NEEDS TO BE MADE GENERIC (into config?), AND DELETED FROM HERE:
-    if systype == "mac":
-        # vim statusline  https://powerline.readthedocs.io/en/latest/installation/osx.html
-        pipinstall("powerline-status", user=True)
-
     # this goes after os specific installs because that bootstraps various vim pluginery
     if redovim == "y":
         # TODO: this should be some kind of user supplied function
         vim()
 
     # Install fzf bindings  TODO: is there a yes/yes/yes option so we can do this automatically?
-    print("******NOTE!!! Manually run the following which are not yet handled******")
+    print("\n\n******NOTE!!! Manually run the following which are not yet handled******")
     print("/usr/local/opt/fzf/install")
     print("vim +GoInstallBinaries +qall")
